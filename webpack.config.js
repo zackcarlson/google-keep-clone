adf0ffa8c2f55c637e319fe7b6d93a2d58c70aab
zackcarlson <zackt.carlson@gmail.com>
2019-03-07 10:18:14 -0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  }
+};
